e78d6021a53d180ee34453449eea11d35b60f506
Patrickdelacruz <75286459+Patrickdelacruz@users.noreply.github.com>
2020-12-14 09:21:57 +0800

    MES-717
    Link Reschedule Delivery Date modal upon drag in Production Scheduling

--- resources/views/reports/tbl_reschedule_delivery_date.blade.php
+++ resources/views/reports/tbl_reschedule_delivery_date.blade.php
@@ -1,16 +1,4 @@
-<div class="row">
-	<div class="col-md-12">
-		<h5 class=" font-weight-bold m-0">{{ $prod_details->sales_order }}{{ $prod_details->material_request }}</h5>
-		<span class="d-block" style="font-size:14px;"><b>{{ $prod_details->customer}}</b> </span>
-
-		<span class="d-block" style="font-size:13px;"><i>{{ $prod_details->production_order }} - {{ $prod_details->item_code }}</i> {{ $prod_details->description }}</span>
-		<span class="d-block" style="font-size:12px;">{{ $prod_details->qty_to_manufacture }}</b> {{ $prod_details->stock_uom }}</span>
-
-
-   </div>
-</div>
-<div class="row">
-   @php
+@php
 	 if($prod_details->rescheduled_delivery_date == null){
 		$deli=  $prod_details->deli;
 	 }else{
@@ -18,12 +6,31 @@
 
 	 }
    @endphp
-   <div class="col-md-6 ">
+
+<div class="row">
+	<div class="col-md-12">
+		<h5 class="font-weight-bold m-0">{{ $prod_details->sales_order }}{{ $prod_details->material_request }} - {{ $prod_details->customer}}</h5>
+		<span class="d-block" style="font-size:17px;">{{ $prod_details->item_code }} - {{ $prod_details->qty_to_manufacture }}</b> {{ $prod_details->stock_uom }}</span>
+		<span class="d-block" style="font-size:17px;"><b>Delivery Date: </b>{{ $deli }}</span>
+   </div>
+</div>
+<div class="row">
+	<input type="hidden" value="{{ $deli }}" name="delivery_date">
+	<input type="hidden" value="{{ $prod_details->production_order }}" name="production_order">
+	<input type="hidden" value="{{ $prod_details->planned_start_date }}" name="planned_start_date">
+	
+   <div class="col-md-12" style="margin-top:15px;">
    		<div class="form-group text-center">
 			<div class="row">
-				<div class="col-md-12">
+				<div class="col-md-6">
 					<div class="form-group">
-                        <label><b>Reschedule Delivery Reason:</b></label>
+                        <label><b>Reschedule Date:</b></label>
+						<input type="date"  class="form-control" name="reschedule_date" style="text-align: center; font-size: 10pt;color:black;" required>					
+                     </div>
+				</div>
+				<div class="col-md-6">
+					<div class="form-group">
+                        <label><b>Reason:</b></label>
                         <select class="form-control" name="reason_id" id="reched_reason" required>
                             @foreach($reason as $row)
                               <option value="{{ $row->reschedule_reason_id }},{{$row->reschedule_reason}}"> {{$row->reschedule_reason}}</option>
@@ -43,25 +50,5 @@
 				</div>
 			</div>
 		</div>
-   </div>
-   <div class="col-md-6">
-		<div class="text-center">
-			<span class="text-center d-block mt-3 mb-3">Current Delivery Date: </b> {{ $deli }}</span>
-			<input type="hidden" value="{{ $deli }}" name="delivery_date">
-			<input type="hidden" value="{{ $prod_details->production_order }}" name="production_order">
-			<input type="hidden" value="{{ $prod_details->planned_start_date }}" name="planned_start_date">
-
-		</div>
-		
-		<div class="form-group text-center">
-			<div class="row">
-				<div class="col-md-8 offset-md-2">
-					<p style="font-size: 11pt;" class="text-center"><b>Reschedule Date</b></p>
-					<input type="date"  class="form-control" name="reschedule_date" style="text-align: center; font-size: 15t;color:black;" required>					
-				</div>
-			</div>
-		</div>
-
-		
    </div>
 </div>
