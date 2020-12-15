a3428300e0f0412498d63aee8c6af1eeb19ae618
clyntonjae <75285893+clyntonjae@users.noreply.github.com>
2020-12-15 14:02:15 +0800

    Revert "Revert MES-717"
    This reverts commit 349514ab4087781177edd49a45144830555189e8.

--- resources/views/reports/item_feedback.blade.php
+++ resources/views/reports/item_feedback.blade.php
@@ -1650,7 +1650,25 @@ $(document).ready(function(){
     });
   }
   
-
+  $('#reschedule_delivery_frm').submit(function(e){
+      e.preventDefault();
+      var url = $(this).attr("action");
+      $.ajax({
+        url: url,
+        type:"POST",
+        data: $(this).serialize(),
+        success:function(data){
+          if (data.success < 1) {
+            showNotification("danger", data.message, "now-ui-icons travel_info");
+          }else{
+            showNotification("success", data.message, "now-ui-icons ui-1_check");
+            $('#reschedule-delivery-modal').modal('hide');
+            load_list();
+          }
+        }
+      });
+    });
+    
   $(document).on('click', '.spotclass', function(event){
     event.preventDefault();
     var jtid = $(this).attr('data-jobticket');
@@ -2079,6 +2097,7 @@ $(document).on('click', '.feedbacked_log_btn', function(){
        },
     });
 });
+
 
 </script>
 @endsection
