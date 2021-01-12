b77f1bd5c8cbca2456c77841d0cc64a5702e858d
clyntonjae <75285893+clyntonjae@users.noreply.github.com>
2021-01-12 16:52:06 +0800

    MES-759
    Set into upper case all data input in Manual Production Order upon saving

--- app/Http/Controllers/ManufacturingController.php
+++ app/Http/Controllers/ManufacturingController.php
@@ -1071,7 +1071,7 @@ class ManufacturingController extends Controller
                 'status' => 'Not Started',
                 'description' => $request->description,
                 'company' => 'FUMACO Inc.',
-                'production_item' => $request->item_code,
+                'production_item' => strtoupper($request->item_code),
                 'sales_order_item' => null,
                 'bom_no' => $request->bom,
                 'wip_warehouse' => $wip,
@@ -1098,9 +1098,9 @@ class ManufacturingController extends Controller
             
             $data_mes = [
                 'production_order' => $new_id,
-                'parent_item_code' => $request->parent_code,
-                'sub_parent_item_code' => ($request->sub_parent_code)? $request->sub_parent_code : $bom_sub_parent,
-                'item_code' => $request->item_code,
+                'parent_item_code' => strtoupper($request->parent_code),
+                'sub_parent_item_code' => strtoupper(($request->sub_parent_code)? $request->sub_parent_code : $bom_sub_parent),
+                'item_code' => strtoupper($request->item_code),
                 'description' => $request->description,
                 'parts_category' => $item_details->parts_category,
                 'item_classification' => $item_classification,
@@ -1133,7 +1133,7 @@ class ManufacturingController extends Controller
             $del_data = [
                 'erp_reference_id' => $request->item_reference_id,
                 'reference_no' => ($sales_order) ? $sales_order : $material_request,
-                'parent_item_code' => $request->parent_code,
+                'parent_item_code' => strtoupper($request->parent_code),
                 'delivery_date' => $request->delivery_date
             ];
 
@@ -1842,7 +1842,7 @@ class ManufacturingController extends Controller
                 $sted_names = array_column($pending_stock_entries, 'sted_name');
                 foreach ($sted_names as $sted_name) {
                     $values = [
-                        'item_code' => $request->item_code,
+                        'item_code' => strtoupper($request->item_code),
                         'item_name' => $request->item_name,
                         'description' => $request->description,
                         'qty' => $request->quantity,
@@ -1863,7 +1863,7 @@ class ManufacturingController extends Controller
             $production_order_item = [
                 'modified' => $now->toDateTimeString(),
                 'modified_by' => Auth::user()->email,
-                'item_code' => $request->item_code,
+                'item_code' => strtoupper($request->item_code),
                 'item_name' => $request->item_name,
                 'description' => $request->description,
                 'required_qty' => $request->quantity,
@@ -2462,7 +2462,7 @@ class ManufacturingController extends Controller
                 'status' => 'Not Started',
                 'description' => $request->description,
                 'company' => 'FUMACO Inc.',
-                'production_item' => $request->item_code,
+                'production_item' => strtoupper($request->item_code),
                 'sales_order_item' => null,
                 'bom_no' => (!$request->custom_bom) ? $request->bom : null,
                 'wip_warehouse' => $wip,
@@ -2479,7 +2479,7 @@ class ManufacturingController extends Controller
                 'order_no' => 0,
                 'priority' => 'Normal',
                 'classification' => $request->classification,
-                'parent_item_code' => $parent_item_code,
+                'parent_item_code' => strtoupper($parent_item_code),
                 'planned_start_date' => ($request->planned_date) ? $request->planned_date : null,
             ];
 
@@ -2488,9 +2488,9 @@ class ManufacturingController extends Controller
 
             $data_mes = [
                 'production_order' => $new_id,
-                'parent_item_code' => $parent_item_code,
-                'sub_parent_item_code' => ($request->sub_parent_code) ? $request->sub_parent_code : $request->item_code,
-                'item_code' => $request->item_code,
+                'parent_item_code' => strtoupper($parent_item_code),
+                'sub_parent_item_code' => strtoupper(($request->sub_parent_code) ? $request->sub_parent_code : $request->item_code),
+                'item_code' => strtoupper($request->item_code),
                 'description' => $request->description,
                 'parts_category' => $item_details->parts_category,
                 'item_classification' => $request->item_classification,
