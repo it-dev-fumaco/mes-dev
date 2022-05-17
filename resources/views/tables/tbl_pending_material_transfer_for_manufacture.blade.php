dc8f0905558684dcf509b148cfd64297d3403d4f
clyntonjae <75285893+clyntonjae@users.noreply.github.com>
2022-05-17 10:35:46 +0800

    MES-980
    Can still click change source warehouse after feedback

--- resources/views/tables/tbl_pending_material_transfer_for_manufacture.blade.php
+++ resources/views/tables/tbl_pending_material_transfer_for_manufacture.blade.php
@@ -275,7 +275,7 @@
 								$change_cancel_btn = ($a['ste_docstatus'] == 1) ? 'disabled' : null;
 								$return_btn = ($a['status'] == 'Issued') ? '' : 'disabled';
 							@endphp
-							<button type="button" class="btn btn-info  btn-sm p-1 change-required-item-btn" data-production-order="{{ $production_order_details->production_order }}" data-item-classification="{{ $component['item_classification'] }}" data-production-order-item-id="{{ $component['name'] }}"> 
+							<button type="button" class="btn btn-info  btn-sm p-1 change-required-item-btn" data-production-order="{{ $production_order_details->production_order }}" data-item-classification="{{ $component['item_classification'] }}" data-production-order-item-id="{{ $component['name'] }}" {{ $change_cancel_btn }}> 
 									<i class="now-ui-icons ui-2_settings-90 d-block"></i><span style="font-size: 7pt;">Change</span>
 							</button>
 							<button type="button" class="btn btn-secondary btn-sm p-1 return-required-item-btn" data-production-order="{{ $production_order_details->production_order }}" data-production-order-item-id="{{ $component['name'] }}" {{ $return_btn }} {{ $no_bom }}>
@@ -491,7 +491,7 @@
 								$change_cancel_btn = ($a['ste_docstatus'] == 1) ? 'disabled' : null;
 								$return_btn = ($a['status'] == 'Issued') ? '' : 'disabled';
 							@endphp
-							<button type="button" class="btn btn-info  btn-sm p-1 change-required-item-btn" data-production-order="{{ $production_order_details->production_order }}" data-item-classification="{{ $part['item_classification'] }}" data-production-order-item-id="{{ $part['name'] }}"> 
+							<button type="button" class="btn btn-info  btn-sm p-1 change-required-item-btn" data-production-order="{{ $production_order_details->production_order }}" data-item-classification="{{ $part['item_classification'] }}" data-production-order-item-id="{{ $part['name'] }}" {{ $change_cancel_btn }}> 
 									<i class="now-ui-icons ui-2_settings-90 d-block"></i><span style="font-size: 7pt;">Change</span>
 							</button>
 							<button type="button" class="btn btn-secondary btn-sm p-1 return-required-item-btn" data-production-order="{{ $production_order_details->production_order }}" data-production-order-item-id="{{ $part['name'] }}" {{ $return_btn }} {{ $no_bom }}>
