fbbe36578c893af998165e468c6fe53341536e4f
clyntonjae <75285893+clyntonjae@users.noreply.github.com>
2022-10-28 16:19:10 +0800

    MES-1193
    Display pre-loader upon printing documents (Withdrawal Slip and Job Ticket)

--- resources/views/production_kanban.blade.php
+++ resources/views/production_kanban.blade.php
@@ -930,10 +930,103 @@
   </div>
 </div>
 
+
+<div id="loader-wrapper" hidden>
+  <div id="loader"></div>
+  <div class="loader-section section-left"></div>
+  <div class="loader-section section-right"></div>
+</div>
+
 @include('modals.modal_select_item_merge')
 @include('modals.select_late_delivery_reason_modal')
 
 <style type="text/css">
+  #loader-wrapper {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		z-index: 99999;
+	}
+	#loader {
+		display: block;
+		position: relative;
+		left: 50%;
+		top: 50%;
+		width: 150px;
+		height: 150px;
+		margin: -75px 0 0 -75px;
+		border-radius: 50%;
+		border: 3px solid transparent;
+		border-top-color: #3498db;
+		-webkit-animation: spin 2s linear infinite; /* Chrome, Opera 15+, Safari 5+ */
+		animation: spin 2s linear infinite; /* Chrome, Firefox 16+, IE 10+, Opera */
+		z-index: 999999;
+	}
+	#loader:before {
+		content: "";
+		position: absolute;
+		top: 5px;
+		left: 5px;
+		right: 5px;
+		bottom: 5px;
+		border-radius: 50%;
+		border: 3px solid transparent;
+		border-top-color: #e74c3c;
+		-webkit-animation: spin 3s linear infinite; /* Chrome, Opera 15+, Safari 5+ */
+		animation: spin 3s linear infinite; /* Chrome, Firefox 16+, IE 10+, Opera */
+	}
+	#loader:after {
+		content: "";
+		position: absolute;
+		top: 15px;
+		left: 15px;
+		right: 15px;
+		bottom: 15px;
+		border-radius: 50%;
+		border: 3px solid transparent;
+		border-top-color: #f9c922;
+		-webkit-animation: spin 1.5s linear infinite; /* Chrome, Opera 15+, Safari 5+ */
+		animation: spin 1.5s linear infinite; /* Chrome, Firefox 16+, IE 10+, Opera */
+	}
+	@-webkit-keyframes spin {
+		0%   { 
+			-webkit-transform: rotate(0deg);  /* Chrome, Opera 15+, Safari 3.1+ */
+			-ms-transform: rotate(0deg);  /* IE 9 */
+			transform: rotate(0deg);  /* Firefox 16+, IE 10+, Opera */
+		}
+		100% {
+			-webkit-transform: rotate(360deg);  /* Chrome, Opera 15+, Safari 3.1+ */
+			-ms-transform: rotate(360deg);  /* IE 9 */
+			transform: rotate(360deg);  /* Firefox 16+, IE 10+, Opera */
+		}
+	}
+	@keyframes spin {
+		0%   { 
+			-webkit-transform: rotate(0deg);  /* Chrome, Opera 15+, Safari 3.1+ */
+			-ms-transform: rotate(0deg);  /* IE 9 */
+			transform: rotate(0deg);  /* Firefox 16+, IE 10+, Opera */
+		}
+		100% {
+			-webkit-transform: rotate(360deg);  /* Chrome, Opera 15+, Safari 3.1+ */
+			-ms-transform: rotate(360deg);  /* IE 9 */
+			transform: rotate(360deg);  /* Firefox 16+, IE 10+, Opera */
+		}
+	}
+	#loader-wrapper .loader-section {
+		position: fixed;
+		top: 0;
+		width: 100%;
+		height: 100%;
+		background-color:  #e5e7e9 ;
+		z-index: 1000;
+		opacity: 50%;
+		-webkit-transform: translateX(0);  /* Chrome, Opera 15+, Safari 3.1+ */
+		-ms-transform: translateX(0);  /* IE 9 */
+		transform: translateX(0);  /* Firefox 16+, IE 10+, Opera */
+	}
+
  #shift-schedule-modal .form-control{
     border: 1px solid #ccc;
     border-radius: 3px;
@@ -2456,6 +2549,7 @@ $(document).on('click', '.printbtnprint', function(){
   if(tryval == ''){
     showNotification("danger", "No selected Production Order", "now-ui-icons travel_info");
   }else{
+    $('#loader-wrapper').removeAttr('hidden');
     
     $.ajax({
     url: "/selected_print_job_tickets/" + tryval,
@@ -2466,6 +2560,7 @@ $(document).on('click', '.printbtnprint', function(){
       }else{
       $('#printmodalbody').html(data);
       $('#print_modal_js_ws').modal('show');
+      $('#loader-wrapper').attr('hidden', true);
       }
     },
     error: function(jqXHR, textStatus, errorThrown) {
@@ -2484,6 +2579,7 @@ $(document).on('click', '.printbtnwidrawal', function(){
   if(tryval == ''){
     showNotification("danger", "No selected Production Order", "now-ui-icons travel_info");
   }else{
+    $('#loader-wrapper').removeAttr('hidden');
     $.ajax({
     url: "/print_withdrawals",
     type:"GET",
@@ -2494,6 +2590,7 @@ $(document).on('click', '.printbtnwidrawal', function(){
       }else{
       $('#printmodalbody').html(data);
       $('#print_modal_js_ws').modal('show');
+      $('#loader-wrapper').attr('hidden', true);
       }
     },
     error: function(jqXHR, textStatus, errorThrown) {
