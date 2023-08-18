ecd5ea9afb8213fb9fe7aebe889356f1967aaeca
javekulong <85483372+javekulong@users.noreply.github.com>
2023-08-18 11:29:31 +0800

    MES-1397
    In Production Settings - Reschedule delivery reason setup, add remove button

--- resources/views/settings/production_settings.blade.php
+++ resources/views/settings/production_settings.blade.php
@@ -322,7 +322,7 @@
 				</div>
 				<div class="modal-footer">
 					<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-					<button type="submit" class="btn btn-primary">Submit</button>
+					<button type="submit" class="btn btn-primary">Delete</button>
 				</div>
 			</div>
 		</form>
