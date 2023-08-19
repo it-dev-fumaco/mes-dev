21cb687d579d90d5c01a5521b3c0858c795d3356
clyntonjae <75285893+clyntonjae@users.noreply.github.com>
2023-08-19 09:33:10 +0800

    Update AuthGates.php

--- app/Http/Middleware/AuthGates.php
+++ app/Http/Middleware/AuthGates.php
@@ -22,13 +22,70 @@ class AuthGates
             $department = DB::connection('mysql_essex')->table('departments')->where('department_id', $user->department_id)->pluck('department')->first();
             $is_admin = $department && $department == 'Information Technology' ? 1 : 0;
 
-            $role_permissions = DB::connection('mysql_mes')->table('role_permissions')
-                ->select('user_group_id', 'permission')->get();
+			$permissionsArray = [];
+			if ($is_admin) {
+				$role_permissions_array = [
+					'manage-workstations',
+					'manage-machines',
+					'manage-rescheduled-delivery-reason',
+					'manage-production-order-cancellation',
+					'manage-shifts',
+					'manage-item-classification-source',
+					'manage-fast-issuance-permission',
+					'manage-wip-warehouse',
+					'manage-users',
+					'manage-user-groups',
+					'manage-email-notifications',
+					'manage-role-permissions',
+					'reports',
+					'view-incoming-orders',
+					'create-production-order',
+					'cancel-production-order',
+					'close-production-order',
+					'override-production-order',
+					'reopen-production-order',
+					'create-production-order-feedback',
+					'cancel-production-order-feedback',
+					'reschedule-delivery-date-order',
+					'create-withdrawal-slip',
+					'print-withdrawal-slip',
+					'change-production-order-items',
+					'fast-issue-items',
+					'return-items-to-warehouse',
+					'add-production-order-items',
+					'create-material-request',
+					'assign-shift-schedule',
+					'reschedule-delivery-date-production-order',
+					'assign-production-order-schedule',
+					'assign-production-order-to-machines',
+					'assign-bom-process',
+					'print-job-ticket',
+					'edit-operator-timelog',
+					'reset-operator-timelog',
+					'override-operator-timelog',
+					'update-wip-production-order-process',
+				];
 
-            $permissionsArray = [];
+				$role_permissions = [];
+				foreach($role_permissions_array as $permission) {
+					$role_permissions[] = [
+						'permission' => $permission,
+						'user_group_id' => $user->user_group_id
+					];
 
-			foreach ($role_permissions as $permission) {
-				$permissionsArray[$permission->permission][] = $permission->user_group_id;
+					$role_permissions = collect($role_permissions);
+				}
+				
+				foreach ($role_permissions as $permission) {
+					$permissionsArray[$permission['permission']][] = $permission['user_group_id'];
+				}
+			} else {
+				$role_permissions = DB::connection('mysql_mes')->table('role_permissions')
+                	->select('user_group_id', 'permission')->get()->toArray();
+
+				foreach ($role_permissions as $permission) {
+					$permissionsArray[$permission->permission][] = $permission->user_group_id;
+				}
 			}
 
 			$user_roles = DB::connection('mysql_mes')->table('user')
