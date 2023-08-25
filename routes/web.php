f55428edc3c15f21c233880cc57f2783b60b9d82
javekulong <85483372+javekulong@users.noreply.github.com>
2023-08-25 13:47:17 +0800

    MES-1405
    In Scheduling - Machine Scheduling, allow re-ordering of machines in the list

--- routes/web.php
+++ routes/web.php
@@ -334,6 +334,7 @@ Route::group(['middleware' => 'auth'], function(){
 	Route::get('/qa_monitoring_summary/{schedule_date}', 'SecondaryController@qa_monitoring_summary');
 	Route::get('/production_schedule_monitoring/{operation}/{schedule_date}', 'MainController@production_schedule_monitoring');
 	Route::get('/production_schedule_monitoring_filters/{operation}/{schedule_date}', 'MainController@production_schedule_monitoring_filters');
+	Route::post('/save_machine_order', 'MainController@save_machine_order');
 	Route::post('/calendar/update_planned_start_date', 'SecondaryController@update_planned_start_date');
     Route::post('/add_shift_schedule', 
         ['uses' => 'SecondaryController@add_shift_schedule', 'as' => 'fabrication.ajax_update']);
