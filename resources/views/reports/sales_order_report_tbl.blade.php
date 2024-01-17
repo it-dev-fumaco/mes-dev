afcde65c8baa7c7e7f43572fca8a0679a156a679
javekulong <85483372+javekulong@users.noreply.github.com>
2024-01-17 11:51:24 +0800

    UI Adjustment

--- resources/views/reports/sales_order_report_tbl.blade.php
+++ resources/views/reports/sales_order_report_tbl.blade.php
@@ -1,18 +1,23 @@
 <table class="table table-bordered">
     <tr>
         <th class="text-center">Name</th>
-        <th class="text-center">Created By</th>
-        <th class="text-center">Created At</th>
         <th class="text-center">Customer</th>
         <th class="text-center">Status</th>
+        <th class="text-center">Created By</th>
+        <th class="text-center">Created At</th>
     </tr>
     @forelse ($sales_orders as $so)
+    @php
+        $created_by = explode('@', $so->owner)[0];
+        $created_by = str_replace('.', ' ', $created_by);
+        $created_by = ucwords($created_by);
+    @endphp
         <tr>
             <td class="text-center">{{ $so->name }}</th>
-            <td class="text-center">{{ $so->owner }}</th>
-            <td class="text-center">{{ Carbon\Carbon::parse($so->creation)->format('M. d, Y - h:i A') }}</th>
             <td class="text-center">{{ $so->customer }}</th>
             <td class="text-center">{{ $so->status }}</th>
+            <td class="text-center">{{ $created_by }}</th>
+            <td class="text-center">{{ Carbon\Carbon::parse($so->creation)->format('M. d, Y - h:i A') }}</th>
         </tr>
     @empty
         <tr>
