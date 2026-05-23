4f4a72c5e0b94a6e39580db3b622d8fef3923a6b
fum-it <75285893+clyntonjae@users.noreply.github.com>
2026-05-23 12:41:18 +0800

    hotfix: enable cancel button in the product oder items

--- resources/views/tables/tbl_production_order_items.blade.php
+++ resources/views/tables/tbl_production_order_items.blade.php
@@ -361,7 +361,7 @@
 						<td class="border-top-0 text-center">
 							@php
 								$change_cancel_btn = ($a['status'] == 'Issued') ? 'disabled' : null;
-								if (!$details->bom_no && $component['item_code'] == $details->item_code && count($component['withdrawals']) == 1) {
+								if (!$details->bom_no && $component['item_code'] == $details->item_code && count($component['withdrawals']) == 1 && $a['issued_qty'] > 0) {
 									$change_cancel_btn = 'disabled';
 								}
 								$return_btn = ($a['status'] == 'Issued') ? '' : 'disabled';
